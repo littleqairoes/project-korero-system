3f7e8259f02f36c23d1459c84da79ac6fb31456a
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-01-30 16:32:08 +0800

    First commit

---
+++ scripts/elements.js
@@ -0,0 +1,13 @@
+PROJECTKORERO-SYSTEM.Elements = (function () {
+  'use strict';
+
+  function init() {
+    PROJECTKORERO-SYSTEM.Elements.Template = document.getElementById('nice-app');
+    PROJECTKORERO-SYSTEM.Elements.Loader = document.getElementById('showbox');
+    
+  }
+
+  return {
+    init: init
+  };
+}());

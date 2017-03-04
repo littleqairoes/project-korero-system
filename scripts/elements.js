93110d319f75dd4db50cd021dd9de50e220077a4
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-03-04 08:09:53 +0800

    Still bugfixing
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- scripts/elements.js
+++ scripts/elements.js
@@ -4,7 +4,16 @@ KORERO.Elements = (function () {
   function init() {
     KORERO.Elements.Template = document.getElementById('core-app');
     KORERO.Elements.Loader = document.getElementById('showbox');
-
+    KORERO.Actions = {
+      SET_INTERFACE_ID: "SET_INTERFACE_ID",
+      OPEN_CREATE_DISCUSSION: "OPEN_CREATE_DISCUSSION",
+      CLOSE_CREATE_DISCUSSION: "CLOSE_CREATE_DISCUSSION",
+      CLOSE_DISCUSSION_TOOLBAR: "CLOSE_DISCUSSION_TOOLBAR",
+      CLOSE_REFER_VIEW: "CLOSE_REFER_VIEW",
+      SET_SELECTION: "SET_SELECTION",
+      SET_NEW_DISCUSSION_LINKS: "SET_NEW_DISCUSSION_LINKS",
+      CLOSE_BOTTOM: "CLOSE_BOTTOM"
+    }
   }
 
   return {
